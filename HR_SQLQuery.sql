25d029d3362cba55533c3742da5d721d19b8812c
Twelany <37784293+Twelany@users.noreply.github.com>
2024-07-15 15:22:19 +0200

    SQL queries used
    SQL queries used

--- HR_SQLQuery.sql
+++ HR_SQLQuery.sql
@@ -1,6 +1,6 @@
 /*
 This project dives deep into the realm of data analysis using SQL and Power BI to uncover important human resource insights that can greatly benefit the company.
-Date: Nov. 28 2023
+Date: Nov. 28 2023 
 Author: Twelane Mudaka
 Get more projects at: http://datascienceportfol.io/Twelanymudaka
 */
